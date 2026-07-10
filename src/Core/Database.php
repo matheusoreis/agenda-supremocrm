767a64ae3509c1e3722cc1fca48f792747cd19ac
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2026-07-10 17:43:14 -0300

    Conector do banco de dados

---
+++ src/Core/Database.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace SupremoCRM\Agenda\Core;
+
+use PDO;
+use PDOException;
+
+class Database
+{
+    private static ?Database $instance = null;
+    private PDO $connection;
+
+    private function __construct()
+    {
+        $config = require __DIR__ . '/../../config/database.php';
+
+        try {
+            $dsn = "mysql:host={$config['host']};dbname={$config['database']};charset={$config['charset']}";
+
+            $this->connection = new PDO(
+                $dsn,
+                $config['username'],
+                $config['password']
+            );
+
+            $this->connection->setAttribute(
+                PDO::ATTR_ERRMODE,
+                PDO::ERRMODE_EXCEPTION
+            );
+        } catch (PDOException $e) {
+            die('Connection failed: ' . $e->getMessage());
+        }
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+}
