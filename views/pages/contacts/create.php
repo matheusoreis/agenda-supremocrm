14e1a267736d469d087d151d3a88f2539735cc3e
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2026-07-11 09:26:11 -0300

    Ajuste no layout para botões md

--- views/pages/contacts/create.php
+++ views/pages/contacts/create.php
@@ -16,7 +16,7 @@ require_once __DIR__ . '/../../../src/Helpers/helpers.php';
 
         <?= render_component('button', [
             'variant' => 'secondary',
-            'size' => 'sm',
+            'size' => 'md',
             'icon' => 'arrow-left',
             'label' => 'Voltar',
             'onclick' => "window.location.href='/contacts'"
